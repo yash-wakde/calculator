3c0d79a291ce7c0916b5a1af42ac49104901ca46
yash-wakde <wakdeyash@gmail.com>
2023-10-01 01:08:26 +0530

    Add operate function

--- script.js
+++ script.js
@@ -1,71 +1,88 @@
 const buttons = document.querySelectorAll('.button')
 const calcArea = document.querySelector('.calc-area')
 const operatorButtons = document.querySelectorAll('.operator-button')
+const equalButton = document.querySelector('.equal-button')
 
 let firstNumber = ''
 let secondNumber = ''
 let currentOperator = ''
 
-buttons.forEach((button)=>{
-    button.addEventListener('click',()=>{
-        if (calcArea.innerText === "0") {
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        if (calcArea.innerText === "0" || calcArea.innerText === "ERROR") {
             calcArea.innerText = button.innerText;
         } else {
             calcArea.innerText += button.innerText;
         }
-    })
-})
 
-
-operatorButtons.forEach((operator)=>{
-    operator.addEventListener('click', ()=>{
-        currentOperator = operator.innerText
-        calcArea.innerText += currentOperator
-    })
-})
-
-
-
-
-
-
-
-let a = ''
-let b = ''
-
-function add(a,b){
-    return a+b
-}
-
-function subtract(a,b){
-    return a-b
-}
-
-function multiply(a,b){
-    return a*b
-}
-
-function divide(a,b){
-    return (a/b)
-}
-
-function operate(operator,a,b){
-    switch (operator){
-        case '+':
-            return add(a,b)
-        case '-':
-            return subtract(a,b)
-        case '*':
-            return multiply(a,b)
-        case '/':
-            if(b == 0){
-                return 'ERROR'
+        if (currentOperator === '') {
+            firstNumber = calcArea.innerText; 
+        } else {
+            if (currentOperator !== '') {
+                secondNumber = calcArea.innerText.slice(firstNumber.length + 1);
             }
-            else{
-                return divide(a,b)
-            }         
-        default:
-            return 'Invalid operator'
+        }
+    });
+});
+
+operatorButtons.forEach((operator) => {
+    operator.addEventListener('click', () => {
+        if (currentOperator === '') {
+            currentOperator = operator.value;
+            opSign = operator.innerText;
+            calcArea.innerText += opSign;
+        }
+    });
+});
+
+equalButton.addEventListener('click', () => {
+    if (firstNumber !== '' && secondNumber !== '' && currentOperator !== '') {
+        const result = operate(currentOperator, parseFloat(firstNumber), parseFloat(secondNumber));
+        if (!isNaN(result)) {
+            calcArea.innerText = result;
+            firstNumber = result;
+            secondNumber = '';
+            currentOperator = '';
+        } else {
+            calcArea.innerText = 'ERROR';
+            firstNumber = '';
+            secondNumber = '';
+            currentOperator = '';
+        }
+    }
+});
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    if (b === 0) {
+        return 'ERROR';
+    } else {
+        return a / b;
     }
 }
 
+function operate(operator, a, b) {
+    switch (operator) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            return divide(a, b);
+        default:
+            return 'Invalid operator';
+    }
+}
